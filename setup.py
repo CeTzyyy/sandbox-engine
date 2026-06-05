39aa933be8a3f5a2012b293ad0aee78610a2301a
CeTzyyy <kvadratikproc@gmail.com>
2026-06-05 22:18:59 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sandbox-engine",
+    version="0.1.0",
+    author="CeTzyyy",
+    description="Эволюционный движок экосистемы на Python",
+    packages=find_packages(),
+    install_requires=[
+        "Pillow",
+    ],
+    python_requires=">=3.11",
+)
