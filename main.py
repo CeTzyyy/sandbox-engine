7ef64292993bee0bf440906a3bc786eb50fc3442
CeTzyyy <kvadratikproc@gmail.com>
2026-07-10 21:25:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2026 CeTzyyy. All rights reserved.
+# Licensed under SandBox Engine License. See LICENSE file in the project root for full license information.
+
 # main_simple.py — базовая симуляция
 from SandBox import SandBox, Peaceful, Predator
 import random
