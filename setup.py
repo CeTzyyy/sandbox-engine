ecde301f8932194ce24914d40e8fc51cbb19d5ba
CeTzyyy <kvadratikproc@gmail.com>
2026-07-10 21:31:22 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version="0.1.0",
     author="CeTzyyy",
     description="Эволюционный движок экосистемы на Python",
+    license="Proprietary / SandBox Engine License", 
     packages=find_packages(),
     install_requires=[
         "Pillow",
